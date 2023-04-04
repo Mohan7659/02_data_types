e6dc3b4fac2b8644271f813a5bf0262f0d1e6625
Mohan7659 <129480563+Mohan7659@users.noreply.github.com>
2023-04-04 11:08:13 +0530

    Add files via upload

---
+++ 2_data_type.py
@@ -0,0 +1,33 @@
+'''
+Date: 29th march 2023
+Discription: Data types of int,float,str 
+'''
+
+'''
+Data types of int
+'''
+print(type(55))
+print(type(44))
+print(type(66))
+print(type(43))
+print(type(49))
+
+'''
+Data types of float
+'''
+
+print(type(22.45))
+print(type(.3565))
+print(type(8997.0))
+print(type(88.987))
+print(type(86.09))
+
+'''
+Data types of str
+'''
+
+print(type('hello'))
+print(type('hii'))
+print(type('heroo'))
+print(type('smash'))
+print(type('python'))
